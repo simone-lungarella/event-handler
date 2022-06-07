f55462228760cd18d2abc7ef7d48d64d13a59413
Vincenzo Ingenito <Vincenzo.Ingenito-CIC@ibm.com>
2022-06-07 21:07:05 +0200

    feat(Db) : Add datasource heroku

--- src/main/java/it/os/event/handler/config/PersistenceJPAConfig.java
+++ src/main/java/it/os/event/handler/config/PersistenceJPAConfig.java
@@ -9,23 +9,38 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import lombok.extern.slf4j.Slf4j;
+
 @Configuration
 @EnableTransactionManagement
+@Slf4j
 public class PersistenceJPAConfig{
 
 	@Autowired
 	private DbCfg dbCFG;
-	
+
 	@Bean
-	public DataSource generateDS() { 
+	@Profile("dev")
+	public DataSource generateDev() {
+		log.info("Sono in dev");
 		return buildDatasource(dbCFG.getUrl(),dbCFG.getUsername(),dbCFG.getPassword(),dbCFG.getDriver());
+	}
+
+	@Bean
+	@Profile("!dev")
+	public DataSource generateProd() {
+		System.out.println("Sono in prod");
+		return buildDatasourceProd(dbCFG.getUrl());
 	}
 
 	/**
@@ -38,6 +53,7 @@ public class PersistenceJPAConfig{
 	 * @return A usable DataSource.
 	 */
 	private DataSource buildDatasource(final String url, final String username, final String pwd, final String driver) {
+		log.info("Datasource dev");
 		DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
 		dataSourceBuilder.url(url);
 		dataSourceBuilder.username(username);
@@ -46,11 +62,19 @@ public class PersistenceJPAConfig{
 		return dataSourceBuilder.build();
 	}
 
+	private HikariDataSource buildDatasourceProd(final String url) {
+		log.info("Datasource prod");
+		HikariConfig config = new HikariConfig();
+		config.setJdbcUrl("jdbc:mysql://b6bbe04e368f6f:00b611db@eu-cdbr-west-03.cleardb.net/heroku_f52c4e58b5c2566?reconnect=true");
+		return new HikariDataSource(config);
+	}
+
 	@Bean
-	@Primary
-	public LocalContainerEntityManagerFactoryBean userEntityManager() {
+	@Profile("dev")
+	public LocalContainerEntityManagerFactoryBean userEntityManagerDev() {
+		log.info("Sono in dev");
 		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-		em.setDataSource(generateDS());
+		em.setDataSource(generateDev());
 		em.setPackagesToScan("it.os.event.handler.entity");
 		HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
 		vendorAdapter.setShowSql(true);
@@ -63,11 +87,39 @@ public class PersistenceJPAConfig{
 		return em;
 	} 
 
-	@Primary
 	@Bean
-	public PlatformTransactionManager userTransactionManager() {
+	@Profile("!dev")
+	public LocalContainerEntityManagerFactoryBean userEntityManagerProd() {
+		log.info("Sono in prod");
+		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+		em.setDataSource(generateProd());
+		em.setPackagesToScan("it.os.event.handler.entity");
+		HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		vendorAdapter.setShowSql(true);
+		em.setJpaVendorAdapter(vendorAdapter);
+		Map<String, Object> properties = new HashMap<>(); 
+
+		properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL55Dialect");
+		properties.put("hibernate.hbm2ddl.auto", "update");
+		em.setJpaPropertyMap(properties);
+		return em;
+	} 
+
+	@Bean
+	@Profile("dev")
+	public PlatformTransactionManager userTransactionManagerDev() {
+		log.info("Sono in dev");
 		JpaTransactionManager transactionManager = new JpaTransactionManager();
-		transactionManager.setEntityManagerFactory(userEntityManager().getObject()); 
+		transactionManager.setEntityManagerFactory(userEntityManagerDev().getObject()); 
+		return transactionManager;
+	}
+
+	@Bean
+	@Profile("!dev")
+	public PlatformTransactionManager userTransactionManagerProd() {
+		log.info("Sono in prod");
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(userEntityManagerProd().getObject()); 
 		return transactionManager;
 	}
 
