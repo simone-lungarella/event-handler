e4186ce1cb1ee4a3ac74dff87b7d4846b1ca7537
Simone <simonelungarella@gmail.com>
2022-06-09 22:52:17 +0200

    Added local postgres db

--- src/main/java/it/os/event/handler/enums/StepTypeEnum.java
+++ src/main/java/it/os/event/handler/enums/StepTypeEnum.java
@@ -6,13 +6,13 @@ public enum StepTypeEnum {
 
     SOPRALLUOGO(1, "Sopralluogo", "Sopralluogo zona cantiere"),
     REDAZIONE_REPORT(2, "Redazione report", "Redazione report precedente ad inizio cantiere"),
-    APERTURA_CANTIERE(3, "Richiesta apertura cantiere", "Invio report a permitting"),
-    PERMITTING(4, "Permitting", "Permitting"),
-    DOCUMENTAZIONE_SICUREZZA(5, "Documentazione sicurezza", "Documentazione sicurezza successiva all'avvio del cantiere"),
-    COMPLETAMENTO_OOCC(6, "Fine attività OOCC", "Completamento attività OOCC"),
-    COMPLETAMENTO_EEMM(7, "Fine attività EEMM", "Completamento attività EEMM"),
-    SMONTAGGIO_PIAZZOLA(8, "Smontaggio piazzola", "Smontaggio piazzola prima del completamento del cantiere"),
-    CME_ODC(9, "CME/ODC", "CME/ODC");
+    CME_ODC(3, "CME/ODC", "CME/ODC"),
+    APERTURA_CANTIERE(4, "Richiesta apertura cantiere", "Invio report a permitting"),
+    PERMITTING(5, "Permitting", "Permitting"),
+    DOCUMENTAZIONE_SICUREZZA(6, "Documentazione sicurezza", "Documentazione sicurezza successiva all'avvio del cantiere"),
+    COMPLETAMENTO_OOCC(7, "Fine attività OOCC", "Completamento attività OOCC"),
+    COMPLETAMENTO_EEMM(8, "Fine attività EEMM", "Completamento attività EEMM"),
+    SMONTAGGIO_PIAZZOLA(9, "Smontaggio piazzola", "Smontaggio piazzola prima del completamento del cantiere");
 
     @Getter
     private Integer order;
