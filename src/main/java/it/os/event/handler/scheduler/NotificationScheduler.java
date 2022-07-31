5dc00cdc648280b551c173719c40859754a367cc
Simone <simonelungarella@gmail.com>
2022-07-31 16:38:46 +0200

    Fixed possible NPE

--- src/main/java/it/os/event/handler/scheduler/NotificationScheduler.java
+++ src/main/java/it/os/event/handler/scheduler/NotificationScheduler.java
@@ -46,7 +46,7 @@ public class NotificationScheduler {
                         days = schedulerCFG.getMegaWThreshold();
                     }
                     
-                    if (LocalDate.parse(event.getStartingDateEEMM()).isBefore(LocalDate.now().minusDays(days))) {
+                    if (event.getStartingDateEEMM() != null && LocalDate.parse(event.getStartingDateEEMM()).isBefore(LocalDate.now().minusDays(days))) {
                         sendMail(event);
                     } else {
                         log.info("Event with id: {} not expired yet, checking again at next schedule", event.getId());
