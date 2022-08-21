add37236dd2488cf4ca485ee1af9b7ae0d19a30d
Simone Lungarella <simonelungarella@gmail.com>
2022-08-21 10:17:49 +0200

    fix: fix after merge

--- src/test/java/it/os/event/handler/EventTest.java
+++ src/test/java/it/os/event/handler/EventTest.java
@@ -58,8 +58,8 @@ class EventTest {
     @Test
     @DisplayName("Test event creation")
     void whenEventIsInserted_shouldBePersisted() {
-        final List<String> operations = Arrays.asList(OperationTypeEnum.GENERATOR_REPLACING.getDescription());
-        final Integer eventId = eventRepo.save(new EventETY("Turbine name", "XXXX", "Test description", 1,
+        final List<String> operations = Arrays.asList(OperationTypeEnum.SOST_GENERATORE.getDescription());
+        final Integer eventId = eventRepo.save(new EventETY("Turbine name", "XXXX", "Test description",
                 TurbinePower.MEGAWATT.getName(), operations, new SimpleDateFormat("dd-MM-yyyy HH:mm").format(new Date()), TurbineStateEnum.MARCHING.getName()));
 
         assertNotNull(eventId, "The event id should not be null");
@@ -69,8 +69,8 @@ class EventTest {
                 "The event inserted should be present in the database");
 
         final boolean isPersisted = eventSrv.insertNewEvent("Turbine name", "XXXX", "eventDescription", 1, TurbinePower.MEGAWATT.getName(),
-                operations, TurbineStateEnum.LIMITED,
-                LocalDate.now(), LocalDate.now());
+                operations, TurbineStateEnum.LIMITED, LocalDate.now(), LocalDate.now());
+                
         assertTrue(isPersisted, "The event should have been persisted");
     }
 
@@ -78,12 +78,11 @@ class EventTest {
     @DisplayName("Test event deletion")
     void whenEventsAreDeleted_shouldBeRemovedFromDb() {
 
-        final List<String> operations = Arrays.asList(OperationTypeEnum.GENERATOR_REPLACING.getDescription());
+        final List<String> operations = Arrays.asList(OperationTypeEnum.SOST_GENERATORE.getDescription());
 
         // Data preparation
         final boolean isPersisted = eventSrv.insertNewEvent("Turbine name", "XXXX", "eventDescription", 1, TurbinePower.MEGAWATT.getName(),
-                operations, TurbineStateEnum.LIMITED,
-                LocalDate.now(), LocalDate.now());
+                operations, TurbineStateEnum.LIMITED, LocalDate.now(), LocalDate.now());
 
         assumeTrue(isPersisted);
         assumeFalse(CollectionUtils.isEmpty(eventSrv.getOrderedEvents()));
@@ -97,11 +96,10 @@ class EventTest {
     @DisplayName("Test step creation")
     void whenEventIsCreated_stepsShouldBeInserted() {
 
-        final List<String> operations = Arrays.asList(OperationTypeEnum.GENERATOR_REPLACING.getDescription());
+        final List<String> operations = Arrays.asList(OperationTypeEnum.SOST_GENERATORE.getDescription());
 
         final boolean isPersisted = eventSrv.insertNewEvent("Turbine name", "XXXX", "eventDescription", 1, TurbinePower.MEGAWATT.getName(),
-                operations, TurbineStateEnum.LIMITED,
-                LocalDate.now(), LocalDate.now());
+                operations, TurbineStateEnum.LIMITED, LocalDate.now(), LocalDate.now());
 
         assumeTrue(isPersisted);
         assumeFalse(CollectionUtils.isEmpty(eventSrv.getOrderedEvents()));
@@ -119,11 +117,10 @@ class EventTest {
     @DisplayName("Test step update")
     void whenStepIsUpdated_databaseShouldBeFullyUpdated() {
 
-        final List<String> operations = Arrays.asList(OperationTypeEnum.GENERATOR_REPLACING.getDescription());
+        final List<String> operations = Arrays.asList(OperationTypeEnum.SOST_GENERATORE.getDescription());
 
         final boolean isPersisted = eventSrv.insertNewEvent("Turbine name", "XXXX", "eventDescription", 1, TurbinePower.MEGAWATT.getName(),
-                operations, TurbineStateEnum.LIMITED,
-                LocalDate.now(), LocalDate.now());
+                operations, TurbineStateEnum.LIMITED, LocalDate.now(), LocalDate.now());
 
         assumeTrue(isPersisted);
         assumeFalse(CollectionUtils.isEmpty(eventSrv.getOrderedEvents()));
--- src/test/java/it/os/event/handler/SchedulerTest.java
+++ src/test/java/it/os/event/handler/SchedulerTest.java
@@ -36,12 +36,10 @@ import it.os.event.handler.service.IStepSRV;
 /**
  * Test class for retention scheduler.
  */
-@SpringBootTest(properties = {
-        "spring.datasource.url=jdbc:postgresql://localHost:5432/event_handler",
-        "spring.datasource.username=postgres",
-        "spring.datasource.password=admin",
-        "mail.to.lower-threshold=lungarellagianfelice@gmail.com,simonelungarella@gmail.com",
-        "mail.to.higher-threshold=lungarellagianfelice@gmail.com,simonelungarella@gmail.com"})
+@SpringBootTest(properties = { 
+    "spring.datasource.url=jdbc:postgresql://localHost:5432/event_handler", 
+    "spring.datasource.username=postgres",
+    "spring.datasource.password=admin", })
 @ActiveProfiles("test")
 class SchedulerTest {
 
@@ -66,11 +64,10 @@ class SchedulerTest {
     @DisplayName("Test retention scheduler")
     void whenRetentionIsExecuted_allExpiredEventShouldBeDeleted() {
 
-        final List<String> operations = Arrays.asList(OperationTypeEnum.GENERATOR_REPLACING.getDescription());
+        final List<String> operations = Arrays.asList(OperationTypeEnum.SOST_GENERATORE.getDescription());
 
         // Data preparation
-        final boolean isInserted = eventSRV.insertNewEvent("Turbine name", "XXXX", "eventDescription", 1,
-                TurbinePower.MEGAWATT.getName(),
+        final boolean isInserted = eventSRV.insertNewEvent("Turbine name", "XXXX", "eventDescription", 1, TurbinePower.MEGAWATT.getName(),
                 operations, TurbineStateEnum.LIMITED, LocalDate.now(), LocalDate.now());
 
         assumeTrue(isInserted, "The event should be inserted to test the scheduler");
@@ -98,61 +95,54 @@ class SchedulerTest {
     void whenEventIsExpired_notificationMustBeSent() {
 
         final String turbineName = "RO01";
-        final List<String> operations = Arrays.asList(OperationTypeEnum.GENERATOR_REPLACING.getDescription());
+        final List<String> operations = Arrays.asList(OperationTypeEnum.SOST_GENERATORE.getDescription());
 
-        final boolean isInserted = eventSRV.insertNewEvent(turbineName, "1982", "Test", 1,
-                TurbinePower.MEGAWATT.getName(), operations, TurbineStateEnum.LIMITED, LocalDate.now().minusDays(1),
-                LocalDate.now());
-
+        final boolean isInserted = eventSRV.insertNewEvent(turbineName, "1982", "Test", 1, 
+            TurbinePower.MEGAWATT.getName(), operations, TurbineStateEnum.LIMITED, LocalDate.now().minusDays(1), LocalDate.now());
+        
         assumeTrue(isInserted, "The event should be inserted to test the scheduler");
 
         List<EventETY> events = eventSRV.getOrderedEvents();
-        EventETY expiredEvent = events.stream().filter(event -> event.getTurbineName().equals(turbineName))
-                .collect(Collectors.toList()).get(0);
+        EventETY expiredEvent = events.stream().filter(event -> event.getTurbineName().equals(turbineName)).collect(Collectors.toList()).get(0);
         assumeFalse(expiredEvent.isMailSent(), "Mail should not have been sent yet");
 
         // Updating permitting date
         final List<StepETY> steps = stepSRV.getAllEventSteps(expiredEvent.getId());
-        for (int i = 1; i <= StepTypeEnum.PERMITTING.getOrder(); i++) {
+        for (int i=1; i<=StepTypeEnum.PERMITTING.getOrder(); i++) {
             steps.get(i).setComplete(true);
             stepSRV.update(steps.get(i));
             eventSRV.updateEventStep(steps.get(i));
         }
-
+        
         given(schedulerCFG.getMegaWThreshold()).willReturn(0);
         assertDoesNotThrow(() -> notificationScheduler.run());
 
         events = eventSRV.getOrderedEvents();
-        expiredEvent = events.stream().filter(event -> event.getTurbineName().equals(turbineName))
-                .collect(Collectors.toList()).get(0);
+        expiredEvent = events.stream().filter(event -> event.getTurbineName().equals(turbineName)).collect(Collectors.toList()).get(0);
         assertTrue(expiredEvent.isMailSent(), "Mail should have been sent");
-
     }
 
     @Test
     @DisplayName("Test notification scheduler with no expired events")
     void givenNonExpiredEvent_shouldNotSendNotification() {
-
+        
         final String turbineName = "Test turbine";
-        final List<String> operations = Arrays.asList(OperationTypeEnum.GENERATOR_REPLACING.getDescription());
+        final List<String> operations = Arrays.asList(OperationTypeEnum.SOST_GENERATORE.getDescription());
 
-        final boolean isInserted = eventSRV.insertNewEvent(turbineName, "XXXX", "Test", 1,
-                TurbinePower.MEGAWATT.getName(), operations, TurbineStateEnum.LIMITED, LocalDate.now(),
-                LocalDate.now());
-
+        final boolean isInserted = eventSRV.insertNewEvent(turbineName, "XXXX", "Test", 1, 
+            TurbinePower.MEGAWATT.getName(), operations, TurbineStateEnum.LIMITED, LocalDate.now(), LocalDate.now());
+        
         assumeTrue(isInserted, "The event should be inserted to test the scheduler");
 
         List<EventETY> events = eventSRV.getOrderedEvents();
-        EventETY expiredEvent = events.stream().filter(event -> event.getTurbineName().equals(turbineName))
-                .collect(Collectors.toList()).get(0);
+        EventETY expiredEvent = events.stream().filter(event -> event.getTurbineName().equals(turbineName)).collect(Collectors.toList()).get(0);
         assumeFalse(expiredEvent.isMailSent(), "Mail should not have been sent yet");
 
         given(schedulerCFG.getMegaWThreshold()).willReturn(5);
         assertDoesNotThrow(() -> notificationScheduler.run());
 
         events = eventSRV.getOrderedEvents();
-        expiredEvent = events.stream().filter(event -> event.getTurbineName().equals(turbineName))
-                .collect(Collectors.toList()).get(0);
+        expiredEvent = events.stream().filter(event -> event.getTurbineName().equals(turbineName)).collect(Collectors.toList()).get(0);
         assertFalse(expiredEvent.isMailSent(), "Mail should not have been sent if expiration is not reached");
     }
 }
