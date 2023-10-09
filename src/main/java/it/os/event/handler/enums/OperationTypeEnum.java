31ec6da3f5da0860230bb2cb1aa831dc16bfc251
Simone Lungarella <simonelungarella@gmail.com>
2023-10-09 18:34:07 +0200

    feat: Added operation type

--- src/main/java/it/os/event/handler/enums/OperationTypeEnum.java
+++ src/main/java/it/os/event/handler/enums/OperationTypeEnum.java
@@ -33,7 +33,8 @@ public enum OperationTypeEnum {
     SOST_PALA("Sostituzione Pala", "Sostituzione pala"),
     SOST_PITCH_GEAR("Sostituzione Pitch Gear", "Sostituzione Pitch Gear"),
     VASCA_STERRATO("Vasca sterrato", "Vasca sterrato"),
-    VASCA_ASFALTO("Vasca asfalto", "Vasca asfalto");
+    VASCA_ASFALTO("Vasca asfalto", "Vasca asfalto"),
+    ISPEZIONE_PALE_UT_CON_PLE("Ispezione pale UT con PLE", "Ispezione pale UT con PLE");
 
     private String name;
     private String description;
